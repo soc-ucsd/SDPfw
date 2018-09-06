f2cc8d916e77e9c14b8486594fec68e00f96f97a
zhengy09 <zhengy093@gmail.com>
2018-09-06 10:13:25 +0100

    Modified test function

--- Test.m
+++ Test.m
@@ -8,7 +8,7 @@ clc;clear
 % ---------------------------------------------------------------------------- %
 % Parameters
 m   = 3;                      % # constraints
-K.s = [4];                    % PSD cones
+K.s = [1000];                    % PSD cones
 bandWidth = [K.s-1];              % bandWidth for SDP cones
 
 
@@ -22,14 +22,27 @@ tsetup = toc(tsetup);
 % alpha = 10;
 % alpha = [2 3 3 2];
 % alpha = [5 3 1 1];
-opts.NoP = K.s ;
+opts.NoP = 20 ;  %% number of partition
 
 [Anew, bnew, cnew, Knew] = FactorWidth(At,b,c,K,opts);
 
-[x,y,info] = sedumi(At,b,c,K);
 
-[x1,y1,info1] = sedumi(Anew,bnew,cnew,Knew);
+%%
+% [x,y,info] = sedumi(At,b,c,K);
+% [x1,y1,info1] = sedumi(Anew,bnew,cnew,Knew);
+% [c'*x,cnew'*x1]
+% [info.wallsec,info1.wallsec]
 
-[c'*x,cnew'*x1]
+%% Using Mosek
+prob0          = convert_sedumi2mosek(At', b, c, K);
+Tmosek         = tic;
+[rcode0, res0] = mosekopt('minimize info', prob0);
+Tmosek0        = toc(Tmosek);
 
-[info.wallsec,info1.wallsec]
+prob1          = convert_sedumi2mosek(Anew', bnew, cnew, Knew);
+Tmosek         = tic;
+[rcode1, res1] = mosekopt('minimize info', prob1);
+Tmosek1        = toc(Tmosek);
+
+[res0.sol.itr.pobjval,res1.sol.itr.pobjval]
+[Tmosek0,Tmosek1]
