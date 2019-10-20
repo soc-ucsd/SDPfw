cbb3f72c4415d9becfc88118f06c36eacebb146c
Jared Miller <jared.fr.miller@gmail.com>
2019-10-20 00:10:47 -0400

    duffing oscillator
    want to make another polynomiala test.
    
    yalmip is crashing when I try and do the duffing oscillator.
    
    I don't have polynomial equations handy for the N-link pendulum

---
+++ decomposed_structured_subset/duffing_test.m
@@ -0,0 +1,31 @@
+N = 10;
+b = (rand(N) + 0.5)/N;
+a = (rand(N,1)+0.5)/N/N;
+y = sdpvar(N, 1);
+
+%Va = a'*(y.^2 / 2 + y.^4/4);
+%Vb = sum(sum(b.*((y*ones(1, N) - ones(N, 1)*y').^4)));
+V = sum(y.^4);
+%V = Va + Vb;
+
+lam = sdpvar(N, 1);
+%g = sdpvar(1, 1);
+g = 2;
+
+mult_term = y.^2;
+%mult_term = y.^2 .* (g-y.^2);
+p = V - lam'*mult_term;
+
+
+
+F = sos(p);
+%obj = -g;
+obj = 0;
+opts_csp = sdpsettings('solver','SEDUMI');
+opts_csp.sos.csp = 0;
+[model, recoverymodel] = export(F , obj, opts_csp);
+K0s = model.K.s;
+
+opts_csp.sos.csp = 1;
+[model_csp, recoverymodel_csp] = export(F , obj, opts_csp);
+Kcsps = model_csp.K.s;
