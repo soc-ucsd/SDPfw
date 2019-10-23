0109ae2aa906d1e0697e17239a172d159d4766a6
Jared Miller <jared.fr.miller@gmail.com>
2019-10-23 15:52:13 -0400

    further work
    giving up on gram sparsity for CLF.
    
    Will try pivoting to model invalidation

--- decomposed_structured_subset/duffing_test.m
+++ decomposed_structured_subset/duffing_test.m
@@ -1,6 +1,6 @@
 N = 5;
 b = (rand(N) + 0.5)/N;
-a = (rand(N,1)+0.5)/N/N;
+a = (rand(N,1)+0.5)/N;
 y = sdpvar(N, 1);
 
 Va = a'*(y.^2 / 2 + y.^4/4);
@@ -10,15 +10,15 @@ V = Va + Vb;
 
 lam = sdpvar(N, 1);
 %g = sdpvar(1, 1);
-%g = 2;
+g = 1.8;
 
-mult_term = y.^2;
-%mult_term = y.^2 .* (g-y.^2);
-%p = V - lam'*mult_term;
+%mult_term = y.^2;
+mult_term = y.^2 .* (g-y.^2);
+p = V - lam'*mult_term;
 
 
 %F = [sos(V); sos(V - lam'*mult_term)];
-F = sos(V);
+F = [sos(p); lam>=0];
 %F = sos(p);
 %obj = -g;
 obj = 0;
@@ -30,3 +30,5 @@ K0s = model.K.s;
 opts_csp.sos.csp = 1;
 [model_csp, recoverymodel_csp] = export(F , obj, opts_csp);
 Kcsps = model_csp.K.s;
+
+sol = optimize(F, obj, opts_csp);
