4f660c098f265aa6b21d6795fc5d18c863d7986c
Jared Miller <jared.fr.miller@gmail.com>
2020-01-08 11:01:32 -0500

    Further cleaning before merge

--- decomposed_structured_subset/img/plot_region.m
+++
@@ -1,4 +0,0 @@
-function ax = plot_region(data, color)
-ax = patch('Faces',1:length(data.a),'Vertices',[data.a, data.b],...
-    'EdgeColor', color, 'FaceColor', color*0.3 + [1; 1; 1]'*0.7, 'LineWidth', 2)
-end
--- decomposed_structured_subset/img/slice_helpers/dd_shuffle.m
+++
@@ -1,9 +0,0 @@
-function D_new  = dd_shuffle(D, L)
-    D_new  = zeros(size(D));
-    for i = 1: size(D, 2)
-         di = D(:, i);
-         Di = v2m(di);
-         Di_out = L * Di * L';
-         D_new(:, i) = m2v(Di_out)
-    end
-end
