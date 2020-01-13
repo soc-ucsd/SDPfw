f36c2f474e382c4de9f5775ff05ddf698916c78a
Jared Miller <jared.fr.miller@gmail.com>
2020-01-13 17:50:16 -0500

    Constrained LR optimization test
    Try to get this working without ruining sparsity. May be difficult.

--- decomposed_structured_subset/csp_decompose.m
+++ decomposed_structured_subset/csp_decompose.m
@@ -2,9 +2,12 @@ lower = sdpvar(1,1);
 rng( 40, 'twister');
 %N = 64;
 %N = 72;
-N = 120;
-%N = 32;
-b = N/6;
+%N = 120;
+%N = 36;
+N = 6;
+b = floor(N/6);
+CONSTRAINED = 1;
+
 x = sdpvar(N, 1);
 
 %f_R  = sum(100*(x(2:end ) - x(1:end-1) .^2).^2 +  (1 -x(1:end-1 )).^2);
@@ -29,7 +32,29 @@ f_Q = mBasis' *Q_P * mBasis;
 %f_Q = 0;
 f = f_R + f_Q;
  
-F = sos(f - lower);
+
+if CONSTRAINED  
+    d_cons = 2;
+    F_psatz = f - lower;
+    %g = [1 + x; 1 - x];
+    %g = [1 - sum(x(1:3).^2), 1 - sum(x(4:6).^2)];
+    g = (1+x).*(1-x);
+    F = [];    
+    s = zeros(size(g));
+    %c = cell(size(g));
+    c = [];
+    for i = 1:length(g)
+        [si, ci] = polynomial(x, d_cons);
+        F = [F; sos(si)];
+        F_psatz = F_psatz - si*g(i);
+        s(i) = si;
+        c = [c; ci];
+    end
+    F = [sos(F_psatz), F];
+else
+    F = sos(f - lower);
+    c = [];
+end
 obj =  -lower;
 
 opts_csp = sdpsettings('solver','SEDUMI');
@@ -37,10 +62,13 @@ opts_csp = sdpsettings('solver','SEDUMI');
 
 %opts_csp = opts;
 opts_csp.sos.csp = 1;
-[model_csp, recoverymodel_csp] = export(F, obj, opts_csp);
+%[model_csp, recoverymodel_csp] = export(F, obj, opts_csp, [lower; c]);
+%[model_csp, recoverymodel_csp] = export(F, obj, opts_csp);
 
+[F_m, obj_m, monomials] = sosmodel(F, obj, opts_csp, [lower; c]);
+[model_csp, recoverymodel_csp] = export(F_m, obj_m, opts_csp);
 %[x_csp, y_csp, info_csp] = sedumi(model_csp.A', model_csp.b, model_csp.C, model_csp.K);
 %cost_csp = model_csp.C'*x_csp;
 Ks = model_csp.K.s';
 mKs = max(model_csp.K.s);
-save('polynomial_big_clique.mat', 'N', 'b', 'model_csp')
+%save('polynomial_big_clique.mat', 'N', 'b', 'model_csp')
